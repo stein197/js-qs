4646bfa887a4f6dd74e13c54c2d78e070db9e6d0
Nail' Gafarov <nil20122013@gmail.com>
2022-04-28 13:33:13 +0500

    Basic testing

--- test.ts
+++ test.ts
@@ -3,11 +3,14 @@ import * as mocha from "mocha";
 import "should";
 
 mocha.describe("toString()", () => {
-
+	mocha.describe("toString({...}, {discardEmpty})", () => {});
+	mocha.describe("toString({...}, {useFlags})", () => {});
+	mocha.describe("toString({...}, {useIndices})", () => {});
 });
 
 mocha.describe("fromString()", () => {
-
+	mocha.describe("fromString({...}, {discardEmpty})", () => {});
+	mocha.describe("fromString({...}, {emptyTypes})", () => {});
 });
 
 // TODO: test url encoding, see npm qs
