ec46c39ab785ee857f79b1ab17e6d414cd15cf85
Nail' Gafarov <nil20122013@gmail.com>
2022-04-28 13:10:38 +0500

    Basic testing setup

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
+import * as qs from ".";
+import * as mocha from "mocha";
+import "should";
+
+mocha.describe("toString()", () => {
+
+});
+
+mocha.describe("fromString()", () => {
+
+});
+
 // TODO: test url encoding, see npm qs
