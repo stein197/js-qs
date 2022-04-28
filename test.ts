3786f9bb9bee4054e2e312cedbe481b198af4a29
Nail' Gafarov <nil20122013@gmail.com>
2022-04-28 14:00:00 +0500

    Added tests

--- test.ts
+++ test.ts
@@ -3,27 +3,36 @@ import * as mocha from "mocha";
 import "should";
 
 mocha.describe("toString()", () => {
-	mocha.it("Should return empty object when the string is empty", () => {});
-	mocha.it("Should not discard empty values", () => {});
-	mocha.it("Should not discard empty values when object is nesting", () => {});
-	mocha.it("Should convert flags to true", () => {});
-	mocha.it("Should use indices when object contains string and numeric keys", () => {});
-	mocha.it("Should not use indices", () => {});
-	mocha.it("Should be correct when the object contains nested arrays", () => {});
-	mocha.it("Should be correct and use indices when the object contains nested arrays with contain objects", () => {});
+	mocha.it.skip("Should return empty string when the object is empty", () => {});
+	mocha.it.skip("Should not discard empty values", () => {});
+	mocha.it.skip("Should not discard empty values when object is nesting", () => {});
+	mocha.it.skip("Should convert flags to true", () => {});
+	mocha.it.skip("Should use indices when object contains string and numeric keys", () => {});
+	mocha.it.skip("Should not use indices", () => {});
+	mocha.it.skip("Should be correct when the object contains nested arrays", () => {});
+	mocha.it.skip("Should be correct and use indices when the object contains nested arrays with contain objects", () => {});
 	
-	mocha.describe("toString({...}, {discardEmpty})", () => {});
-	mocha.describe("toString({...}, {useFlags})", () => {});
-	mocha.describe("toString({...}, {useIndices})", () => {});
+	mocha.describe.skip("toString({...}, {discardEmpty})", () => {});
+	mocha.describe.skip("toString({...}, {useFlags})", () => {});
+	mocha.describe.skip("toString({...}, {useIndices})", () => {});
 });
 
 mocha.describe("fromString()", () => {
-	mocha.describe("fromString({...}, {discardEmpty})", () => {});
-	mocha.describe("fromString({...}, {emptyTypes})", () => {});
+	mocha.it.skip("Should return empty object when the string is empty", () => {});
+	mocha.it.skip("Should discard leading question mark", () => {});
+	mocha.it.skip("Should discard edge ampersands", () => {});
+	mocha.it.skip("Should discard multiple ampersands", () => {});
+	mocha.it.skip("Should preserve keys with empty values", () => {});
+	mocha.it.skip("Should return true for flags", () => {});
+	mocha.it.skip("Should return the last value for multiple key occurences", () => {});
+	mocha.it.skip("Should return object for strings \"a[b]=1&a[]=2\" and produce consequential numeric keys for empty keys", () => {});
+
+	mocha.describe.skip("fromString({...}, {discardEmpty})", () => {});
+	mocha.describe.skip("fromString({...}, {inferTypes})", () => {});
 });
 
 mocha.describe("toString(...) === fromString(...)", () => {
-	
+
 });
 
 // TODO: test url encoding, see npm qs
