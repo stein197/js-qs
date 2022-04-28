5b0052cb2000018b7f1de005871c21376b9eb250
Nail' Gafarov <nil20122013@gmail.com>
2022-04-28 13:41:12 +0500

    Basic testing

--- test.ts
+++ test.ts
@@ -3,6 +3,15 @@ import * as mocha from "mocha";
 import "should";
 
 mocha.describe("toString()", () => {
+	mocha.it("Should return empty object when the string is empty", () => {});
+	mocha.it("Should not discard empty values", () => {});
+	mocha.it("Should not discard empty values when object is nesting", () => {});
+	mocha.it("Should convert flags to true", () => {});
+	mocha.it("Should use indices when object contains string and numeric keys", () => {});
+	mocha.it("Should not use indices", () => {});
+	mocha.it("Should be correct when the object contains nested arrays", () => {});
+	mocha.it("Should be correct and use indices when the object contains nested arrays with contain objects", () => {});
+	
 	mocha.describe("toString({...}, {discardEmpty})", () => {});
 	mocha.describe("toString({...}, {useFlags})", () => {});
 	mocha.describe("toString({...}, {useIndices})", () => {});
@@ -11,6 +20,10 @@ mocha.describe("toString()", () => {
 mocha.describe("fromString()", () => {
 	mocha.describe("fromString({...}, {discardEmpty})", () => {});
 	mocha.describe("fromString({...}, {emptyTypes})", () => {});
+});
+
+mocha.describe("toString(...) === fromString(...)", () => {
+	
 });
 
 // TODO: test url encoding, see npm qs
