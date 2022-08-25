b63943317f2176b075faae04deee00afc5f986ce
Nail Gafarov <nil20122013@gmail.com>
2022-08-25 17:06:44 +0500

    Added test stubs for stringify()

--- test.ts
+++ test.ts
@@ -1,45 +1,72 @@
+import assert from "assert";
+import mocha from "mocha";
 import * as qs from ".";
-import * as mocha from "mocha";
-
-/*-------------------------------------------------------
-stringify() should be tested against:
-- Empty object
-- Empty array
-- Empty values (null, "", [], {})
-- Simple object
-- Simple array
-- Nested arrays
-- Nested objects
-- Nested array in objects
-- Nested objects in arrays
-stringify() should be tested with options:
-- preserveEmpty
-- indices
-- flags
-parse() should be tested against:
-- Empty string
-- Simple string
-- String with no values for keys
-- String with flags
-- String with numeric and string keys for a single entry
-parse() should be tested with options:
-- preserveEmpty
-- scalars
--------------------------------------------------------*/
 
 mocha.describe("stringify()", () => {
-	mocha.it.skip("Should return empty string when the object is empty", () => {});
-	mocha.it.skip("Should not discard empty values", () => {});
-	mocha.it.skip("Should not discard empty values when object is nesting", () => {});
-	mocha.it.skip("Should convert flags to true", () => {});
-	mocha.it.skip("Should use indices when object contains string and numeric keys", () => {});
-	mocha.it.skip("Should not use indices", () => {});
-	mocha.it.skip("Should be correct when the object contains nested arrays", () => {});
-	mocha.it.skip("Should be correct and use indices when the object contains nested arrays with contain objects", () => {});
+	mocha.it.skip("Should not emit indices for arrays by default when options aren't present");
+	mocha.it.skip("Should not stringify flags by default when options aren't present");
+	mocha.it.skip("Should discard empty values by default when options aren't present");
+	mocha.it.skip("Should discard null and undefined by default when options aren't present");
+	mocha.it.skip("Should not encode keys by default when options aren't present");
+	mocha.it.skip("Should not encode values by default when options aren't present");
+	mocha.it.skip("Should encode special characters in keys");
+	mocha.it.skip("Should encode special characters in values");
+	mocha.it.skip("Should return correct result when passing large complex object with custom options");
 
-	mocha.describe.skip("stringify({...}, {preserveEmpty})", () => {});
-	mocha.describe.skip("stringify({...}, {flags})", () => {});
-	mocha.describe.skip("stringify({...}, {indices})", () => {});
+	mocha.describe("Options", () => {
+		mocha.describe("\"preserveEmpty\"", () => {
+			mocha.it.skip("Should discard empty values when \"preserveEmpty\" is false");
+			mocha.it.skip("Should preserve empty values when \"preserveEmpty\" is true");
+		});
+		mocha.describe("\"indices\"", () => {
+			mocha.it.skip("Should not emit indices for arrays when \"indices\" is false");
+			mocha.it.skip("Should emit indices for arrays when \"indices\" is true");
+		});
+		mocha.describe("\"flags\"", () => {
+			mocha.it.skip("Should stringify flags when \"flags\" is false");
+			mocha.it.skip("Should not stringify flags when \"flags\" is true");
+		});
+		mocha.describe("\"nulls\"", () => {
+			mocha.it.skip("Should discard null and undefined when \"nulls\" is false");
+			mocha.it.skip("Should stringify null and undefined when \"nulls\" is true");
+		});
+		mocha.describe("\"encodeKeys\"", () => {
+			mocha.it.skip("Should not encode keys when \"encodeKeys\" is false");
+			mocha.it.skip("Should encode keys when \"encodeKeys\" is true");
+			mocha.it.skip("Should encode special characters in keys anyway");
+		});
+		mocha.describe("\"encodeValues\"", () => {
+			mocha.it.skip("Should not encode values when \"encodeValues\" is false");
+			mocha.it.skip("Should encode values when \"encodeValues\" is true");
+			mocha.it.skip("Should encode special characters in values anyway");
+		});
+	});
+
+	mocha.describe("Plain arrays", () => {
+		mocha.it.skip("Should return correct result when passing a common array");
+		mocha.it.skip("Should return empty string when the array is empty");
+		mocha.it.skip("Should discard empty values when the array is sparsed");
+	});
+	mocha.describe("Plain objects", () => {
+		mocha.it.skip("Should return correct result when passing a common object");
+		mocha.it.skip("Should return empty string when the object is empty");
+	});
+	mocha.describe("Arrays in arrays", () => {
+		mocha.it.skip("Should return correct result when passing an array in array");
+		mocha.it.skip("Should return empty string when the arrays are empty");
+	});
+	mocha.describe("Objects in arrays", () => {
+		mocha.it.skip("Should return correct result when passing an object in array");
+		mocha.it.skip("Should return empty string when object and array are empty");
+	});
+	mocha.describe("Objects in objects", () => {
+		mocha.it.skip("Should return correct result when passing an object in object");
+		mocha.it.skip("Should return empty string when objects are empty");
+	});
+	mocha.describe("Arrays in objects", () => {
+		mocha.it.skip("Should return correct result when passing an arrasy in object");
+		mocha.it.skip("Should return empty string when arrays and objects are empty");
+	});
 });
 
 // TODO: Populate with more tests
