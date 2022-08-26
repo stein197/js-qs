465788ae45840073e060f17abc16a11c6e7c62a8
Nail Gafarov <nil20122013@gmail.com>
2022-08-26 21:56:46 +0500

    Implemented tests for stringify() function

--- test.ts
+++ test.ts
@@ -3,43 +3,114 @@ import * as mocha from "mocha";
 import * as qs from ".";
 
 mocha.describe("stringify()", () => {
-	mocha.it.skip("Should not emit indices for arrays by default when options aren't present");
-	mocha.it.skip("Should not stringify flags by default when options aren't present");
-	mocha.it.skip("Should discard empty values by default when options aren't present");
-	mocha.it.skip("Should discard null and undefined by default when options aren't present");
-	mocha.it.skip("Should not encode keys by default when options aren't present");
-	mocha.it.skip("Should not encode values by default when options aren't present");
-	mocha.it.skip("Should encode special characters in keys");
-	mocha.it.skip("Should encode special characters in values");
-	mocha.it.skip("Should not encode brackets");
-	mocha.it.skip("Should return correct result when passing large complex object with custom options");
+	mocha.it("Should not emit indices for arrays by default when options aren't present", () => {
+		assert.equal(qs.stringify({a: [1, 2]}), "a[]=1&a[]=2");
+	});
+	mocha.it("Should not stringify flags by default when options aren't present", () => {
+		assert.equal(qs.stringify({a: true}), "a");
+	});
+	mocha.it("Should discard empty values by default when options aren't present", () => {
+		assert.equal(qs.stringify({a: "", b: [], c: {}}), "");
+	});
+	mocha.it("Should discard null and undefined by default when options aren't present", () => {
+		// @ts-ignore
+		assert.equal(qs.stringify({a: null, b: undefined}), "");
+	});
+	mocha.it("Should not encode keys by default when options aren't present", () => {
+		assert.equal(qs.stringify({" ": 1}), " =1")
+	});
+	mocha.it("Should not encode values by default when options aren't present", () => {
+		assert.equal(qs.stringify({a: " "}), "a= ");
+	});
+	mocha.it("Should encode special characters in keys", () => {
+		assert.equal(qs.stringify({"&=[]": 1}), "%26%3D%5B%5D=1");
+	});
+	mocha.it("Should encode special characters in values", () => {
+		assert.equal(qs.stringify({"a": "&=[]"}), "a=%26%3D%5B%5D");
+	});
+	mocha.it("Should not encode brackets", () => {
+		assert.equal(qs.stringify({a: {b: 1}}), "a[b]=1");
+	});
+	mocha.it("Should encode encoded characters in keys", () => {
+		assert.equal(qs.stringify({"%25": 1}), "%2525=1");
+	});
+	mocha.it("Should encode encoded characters in values", () => {
+		assert.equal(qs.stringify({a: "%25"}), "a=%2525");
+	});
+	mocha.it("Should return correct result when passing large complex object with custom options", () => {
+		assert.equal(qs.stringify({
+			a: {
+				b: [
+					"c", {
+						d: 4
+					}
+				]
+			},
+			b: [
+				1, 2
+			],
+			"=": "=",
+			"%25": "%25",
+			"key": "value"
+		}), "a[b][]=c&a[b][][d]=4&b[]=1&b[]=2&%3D=%3D&%2525=%2525&key=value");
+	});
 
 	mocha.describe("Options", () => {
 		mocha.describe("\"preserveEmpty\"", () => {
-			mocha.it.skip("Should discard empty values when \"preserveEmpty\" is false");
-			mocha.it.skip("Should preserve empty values when \"preserveEmpty\" is true");
+			mocha.it("Should discard empty values when \"preserveEmpty\" is false", () => {
+				assert.equal(qs.stringify({a: "", b: [], c: {}}, {preserveEmpty: false}), "");
+			});
+			mocha.it("Should preserve empty values when \"preserveEmpty\" is true", () => {
+				assert.equal(qs.stringify({abc: "", def: [], ghi: {}}, {preserveEmpty: true}), "abc=&def=&ghi=");
+			});
 		});
 		mocha.describe("\"indices\"", () => {
-			mocha.it.skip("Should not emit indices for arrays when \"indices\" is false");
-			mocha.it.skip("Should emit indices for arrays when \"indices\" is true");
+			mocha.it("Should not emit indices for arrays when \"indices\" is false", () => {
+				assert.equal(qs.stringify({a: [1, 2, 3]}, {indices: false}), "a[]=1&a[]=2&a[]=3");
+			});
+			mocha.it("Should emit indices for arrays when \"indices\" is true", () => {
+				assert.equal(qs.stringify({a: [1, 2, 3]}, {indices: true}), "a[0]=1&a[1]=2&a[2]=3");
+			});
 		});
 		mocha.describe("\"flags\"", () => {
-			mocha.it.skip("Should stringify flags when \"flags\" is false");
-			mocha.it.skip("Should not stringify flags when \"flags\" is true");
+			mocha.it("Should stringify flags when \"flags\" is false", () => {
+				assert.equal(qs.stringify({a: true}, {flags: false}), "a=true");
+			});
+			mocha.it("Should not stringify flags when \"flags\" is true", () => {
+				assert.equal(qs.stringify({a: true}, {flags: true}), "a");
+			});
 		});
 		mocha.describe("\"nulls\"", () => {
-			mocha.it.skip("Should discard null and undefined when \"nulls\" is false");
-			mocha.it.skip("Should stringify null and undefined when \"nulls\" is true");
+			mocha.it("Should discard null and undefined when \"nulls\" is false", () => {
+				// @ts-ignore
+				assert.equal(qs.stringify({a: null, b: undefined}, {nulls: false}), "");
+			});
+			mocha.it("Should stringify null and undefined when \"nulls\" is true", () => {
+				// @ts-ignore
+				assert.equal(qs.stringify({a: null, b: undefined}, {nulls: true}), "a=null&b=undefined");
+			});
 		});
 		mocha.describe("\"encodeKeys\"", () => {
-			mocha.it.skip("Should not encode keys when \"encodeKeys\" is false");
-			mocha.it.skip("Should encode keys when \"encodeKeys\" is true");
-			mocha.it.skip("Should encode special characters in keys anyway");
+			mocha.it("Should not encode keys when \"encodeKeys\" is false", () => {
+				assert.equal(qs.stringify({" ": 1}, {encodeKeys: false}), " =1");
+			});
+			mocha.it("Should encode keys when \"encodeKeys\" is true", () => {
+				assert.equal(qs.stringify({" ": 1}, {encodeKeys: true}), "%20=1");
+			});
+			mocha.it("Should encode special characters in keys anyway", () => {
+				assert.equal(qs.stringify({"&[]=%": 1}, {encodeKeys: false}), "%26%5B%5D%3D%25=1");
+			});
 		});
 		mocha.describe("\"encodeValues\"", () => {
-			mocha.it.skip("Should not encode values when \"encodeValues\" is false");
-			mocha.it.skip("Should encode values when \"encodeValues\" is true");
-			mocha.it.skip("Should encode special characters in values anyway");
+			mocha.it("Should not encode values when \"encodeValues\" is false", () => {
+				assert.equal(qs.stringify({a: " "}, {encodeValues: false}), "a= ");
+			});
+			mocha.it("Should encode values when \"encodeValues\" is true", () => {
+				assert.equal(qs.stringify({a: " "}, {encodeValues: true}), "a=%20");
+			});
+			mocha.it("Should encode special characters in values anyway", () => {
+				assert.equal(qs.stringify({a: "&[]=%"}, {encodeValues: false}), "a=%26%5B%5D%3D%25");
+			});
 		});
 	});
 
